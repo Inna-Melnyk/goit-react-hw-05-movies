5099007cf7a35302835194f2154db35ea2845c2b
Inna-Melnyk <116845903+Inna-Melnyk@users.noreply.github.com>
2023-06-28 11:15:04 +0000

    Deploying to gh-pages from @ Inna-Melnyk/goit-react-hw-05-movies@e0efd38f854f6e5cd184fed6fdcccda6382a879b 🚀

---
+++ static/js/805.9e32dc67.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkreact_homework_template=self.webpackChunkreact_homework_template||[]).push([[805],{9805:function(e,t,r){r.r(t);var a=r(184);t.default=function(){return(0,a.jsx)("div",{children:"NotFound Page"})}}}]);
+//# sourceMappingURL=805.9e32dc67.chunk.js.map
